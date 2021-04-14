c018e6c181d75e85be3bf44e17d74c9999c06672
Preethi <preethi@gmail.com>
2021-04-14 10:14:37 +0530

    new App1

--- MavenDemo/src/main/java/com/hcl/MavenDemo/App.java
+++ MavenDemo/src/main/java/com/hcl/MavenDemo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-System.out.println("git uploaded");
+        System.out.println("Hello MathUtil");
     }
 }
---
+++ MavenDemo/src/main/java/com/hcl/MavenDemo/App1.java
@@ -0,0 +1,14 @@
+package com.hcl.MavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void m1( )
+    {
+        System.out.println( "M1 call" );
+        
+    }
+}
